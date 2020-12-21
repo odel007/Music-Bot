51a8438839182a7323f94eb22a9459816d501e5c
odel007 <60863567+odel007@users.noreply.github.com>
2020-12-21 15:17:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const { Client, Util, MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
 const Youtube = require(`simple-youtube-api`)
-const PREFIX = "1";
+const PREFIX = "~";
 
 const client = new Client({ disableEveryone: true });
 
-const youtube = new Youtube("AIzaSyAGmpS9qDMwLgOU1qhSlfwTaJUPFP0NBfY")
+const youtube = new Youtube("AIzaSyB1Z8EkjZ9Xf4QyNqQFe1rXGPokMqFwr1c");
+//const youtube = new Youtube("AIzaSyAGmpS9qDMwLgOU1qhSlfwTaJUPFP0NBfY");
 
-const queue = new Map()
+const queue = new Map();
 
 client.on("ready", () => {
-    console.log("DONE!")
-    client.user.setActivity("MUSIC IN TEST SERVER", ({ type: "PLAYING" }));
+    console.log(`${client.user.usernmae} is online`)
+    client.user.setActivity("⛧MUSIC⛧")
+});
+
+client.on("message", async message => {
+    if(message.content.startsWith(`${PREFIX}ping`)) {
+    const aA = await message.channel.send(`Pinging...`);
+
+    aA.edit(`Pong!
+    latency is ${Math.floor(aA.createdTimestamp - message.createdTimestamp)}ms Api Latency is ${Math.round(client.ping)}ms`);
+}
 })
 
 client.on('message', async message => {
@@ -25,10 +35,10 @@ client.on('message', async message => {
 
     if(message.content.startsWith(`${PREFIX}play`)) {
         const voiceChannel = message.member.voice.channel
-        if(!voiceChannel) return message.channel.send("You need to be in a voice channel to play music!")
+        if(!voiceChannel) return message.channel.send("lu perlu masuk voice channel dulu tolol!")
         const permission = voiceChannel.permissionsFor(message.client.user)
-        if(!permission.has('CONNECT')) return message.channel.send("I don\'t have the permission to speak in the voice channel you\'re at right now!")
-        if(!permission.has("SPEAK")) return message.channel.send("I don\'t have the permission to play music in the voice channnel you\'re at now!")
+        if(!permission.has('CONNECT')) return message.channel.send("Aing gak punya permission mauk kesana njeng!")
+        if(!permission.has("SPEAK")) return message.channel.send("Aing gak punya permission mauk kesana njeng!")
 
         try {
             var video = await youtube.getVideoByID(url)
@@ -37,7 +47,7 @@ client.on('message', async message => {
                 var videos = await youtube.searchVideos(searchString, 1)
                 var video = await youtube.getVideoByID(videos[0].id)
             } catch {
-                return message.channel.send('I could not find any search results!')
+                return message.channel.send('gada!')
             }
         }
 
@@ -71,30 +81,30 @@ client.on('message', async message => {
             }
         } else {
             serverQueue.songs.push(song)
-            return message.channel.send(`**${song.title}** has been added to the queue!`)
+            return message.channel.send(`**${song.title}** dah gw tambahin ke queue ya SU!`)
         }
         return undefined
     } else if (message.content.startsWith(`${PREFIX}stop`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice chanel that I\'m at to stop playing music!")
+        if(!message.member.voice.channel) return message.channel.send("Lu perlu masuk dulu ke voice channel ya njeng!")
         if(!serverQueue) return message.channel.send("There is nothing playing!")
         serverQueue.songs = []
         serverQueue.connection.dispatcher.end()
-        message.channel.send("I have stoppped the music for you!")
+        message.channel.send("dah gw stop musiknya ya su!")
         return undefined
     } else if (message.content.startsWith(`${PREFIX}skip`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel where I\'m at to skip music!")
+        if(!message.member.voice.channel) return message.channel.send("Lu perlu masuk dulu ke voice channel ya njeng!!")
         if(!serverQueue) return message.channel.send("There is nothing playing!")
         serverQueue.connection.dispatcher.end()
-        message.channel.send("Successfully skipped the music!")
+        message.channel.send("dah gw skip musiknya ya su!")
         return undefined
     } else if (message.content.startsWith(`${PREFIX}volume`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel to set the volume!")
+        if(!message.member.voice.channel) return message.channel.send("Lu perlu masuk dulu ke voice channel ya njeng!!")
         if(!serverQueue) return message.channel.send("There is nothing playing in the queue!")
         if(!args[1]) return message.channel.send(`That volume is **${serverQueue.volume}**`)
         if(isNaN(args[1])) return message.channel.send("That is not a valid volume to change to!")
         serverQueue.volume = args[1]
         serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1]/ 5)
-        message.channel.send(`Sucessfully set the volume to **${args[1]}**`)
+        message.channel.send(`dah gw set volumenya jadi **${args[1]}**`)
         return undefined
     } else if (message.content.startsWith(`${PREFIX}now playing`)) {
         if(!serverQueue) return message.channel.send("There is nothing playing!")
@@ -109,26 +119,26 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
         `, { split: true})
         return undefined
     } else if (message.content.startsWith(`${PREFIX}pause`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel where I\'m at to pause the music I\'m playing!")
+        if(!message.member.voice.channel) return message.channel.send("Lu perlu masuk dulu ke voice channel ya njeng!!")
         if(!serverQueue) return message.channel.send("There is nothing playing!")
         if(!serverQueue.playing) return message.channel.send("The music is already paused!")
         serverQueue.playing = false
         serverQueue.connection.dispatcher.pause()
-        message.channel.send("I have paused the music for you!")
+        message.channel.send("dah gw pause musiknya ya njeng!")
         return undefined
     } else if (message.content.startsWith(`${PREFIX}resume`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel where I\'m at to resume the music I\'m playing!")
+        if(!message.member.voice.channel) return message.channel.send("Lu perlu masuk dulu ke voice channel ya njeng!!")
         if(!serverQueue) return message.channel.send("There is nothing playing!")
-        if(serverQueue.playing) return message.channel.send("The music is already resume! Or the music is playing and you send the resume command which totally doesn\'t make sense lol :rofl:")
+        if(serverQueue.playing) return message.channel.send("kan udah sedang di play musiknya njeng ngapa lu resume lagi?")
         serverQueue.playing = true
         serverQueue.connection.dispatcher.resume()
-        message.channel.send("I have paused the music for you!")
+        message.channel.send("dah gw resume musiknya ya njeng!")
         return undefined
     } else if (message.content.startsWith(`${PREFIX}help`)) {
         const meessdd = new MessageEmbed()
         .setColor("RANDOM")
-        .setTitle("Help commands for SCRFC server Music 🎵🎵🎵")
-        .addField('Music commands!', '`1play <song name>`, `1stop`, `1skip`, `1pause`, `1resume`, `1queue`, `1now playing`, `1volume`, `TO DISCONNECT ME MAKE SURE THERES NO QUEUE IF NOT YOU WILL IN TROUBLE`')
+        .setTitle("Help commands for Odell 🎵🎵🎵")
+        .addField('Music commands!', '`~play <song name>`, `~stop`, `~skip`, `~pause`, `~resume`, `~queue`, `~now playing`, `~volume`, `TO DISCONNECT ME MAKE SURE THERES NO QUEUE IF NOT YOU WILL IN TROUBLE`')
         message.channel.send(meessdd)
         return undefined
     }
@@ -156,4 +166,18 @@ function play(guild, song) {
         serverQueue.textchannel.send(` Start playing: **${song.title}**`)
 }
 
-client.login("TOKEN")
+client.on("message", async message => {
+    if(message.content.startsWith(`${PREFIX}hello`)) {
+        message.channel.send(`apa?`)
+    }
+
+})
+
+client.on("message", async message => {
+    if(message.content.startsWith(`${PREFIX}anjing`)) {
+        message.channel.send(`apa lo ngentod?`)
+    }
+
+})
+
+client.login("Nzg5NzczOTk2NzYxMTUzNTQ2.X928Wg.PiSxOVud5zMDrSv8byNLPdBte1g")
